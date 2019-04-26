2f2850afaccf9c7f287447ab71a5d63fedbb03ae
Rostik <rostik1804@gmail.com>
2019-04-26 20:51:17 +0300

    first commit!

---
+++ MVC_tryTask/Models/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_tryTask.Models
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ MVC_tryTask/Models/WorkerContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_tryTask.Models
+{
+    public class WorkerContext : DbContext
+    {
+        public DbSet<Worker> Workers { get; set; }
+    }
+}
